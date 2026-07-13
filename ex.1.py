20d2f0f9303a42440607b6fe3f75b938d0884ce5
5797-tech <5797@eclisboa.net>
2026-07-13 11:44:18 +0100

    Add files via upload

---
+++ ex.1.py
@@ -0,0 +1,42 @@
+# Variáveis
+mensagem1 = "Hello World!"
+mensagem2 = "Bem-vindo ao 'Python'"
+
+numero1 = 6
+numero2 = 4
+
+# Operações
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+divisao_inteira = numero1 // numero2
+resto = numero1 % numero2
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+potencia = numero1 ** numero2
+
+# Substrings
+texto1 = mensagem1[2:10]      # llo Worl
+texto2 = mensagem2[11:19]     # Bem-vindo ao 'Pytho
+
+# Output
+print(mensagem1)
+print(mensagem2)
+
+print("Número 1:", numero1)
+print("Número 2:", numero2)
+
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Divisão sem parte decimal:", divisao_inteira)
+print("Resto da Divisão:", resto)
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Potência:", potencia)
+
+print()
+
+print(texto1)
+
+print()
+
+print(texto2)
